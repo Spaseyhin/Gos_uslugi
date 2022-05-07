882be7077730deb575716f7110a10ec3e62be68c
uwu <spaseyhin>
2022-05-07 23:22:41 +0500

    добавлено называния в сылках

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,11 +3,11 @@
 module ApplicationHelper
   # титул полученый из вьюхи
   def full_title(page_title = '')
-    base_title = '' # базовый титул
+    base_title = 'RKS-Samara' # базовый титул
     if page_title.empty? # если титула в вьюхе нет то
       base_title # базовый титул
     else # иначе
-      "#{page_title}  #{base_title}" # титул из вьюхи и базовый
+      "#{page_title} | #{base_title}" # титул из вьюхи и базовый
     end
   end
 end
